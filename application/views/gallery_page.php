aa1c3b2f0b3e84dfbca348320a1ce5d37c0ed4af
i3rito <matheusi3rito@gmail.com>
2019-10-09 11:30:11 -0300

    Configura para o template.

--- application/views/gallery_page.php
+++ application/views/gallery_page.php
@@ -1,89 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<title>Projeto Galeria</title>
-		<script src="/assets/js/jquery.min.js"></script>
-		<style>
-			.item-galeria{
-				display: none;
-			}
-			.active {
-				border: 1px solid red;
-				display: inline;
-			}
-			#viewport{
-				height: 172px;
-				width: 300px;
-			}
-		</style>
-	</head>
-	<body>
-
-		<div id="viewport">
+	<div id="viewport">
 		<!-- Imagens -->
-			<div>
-				<?php foreach($imagens as $key => $imagem) { ?>
-					<img src="<?php echo $imagem; ?>" class="item-galeria <?php echo ($key==0) ? 'active' : ''; ?>"  style="width: 300px;"/>
-				<?php } ?>
-			</div>
-		</div>
-
-		<!-- buttons -->
 		<div>
-			<img src="<?php echo($botoes[0]); ?>" style="width:50px" class="botao-voltar">
-			<img src="<?php echo($botoes[1]); ?>" style="width:50px" class="botao-avancar">
+			<?php foreach($imagens as $key => $imagem) { ?>
+				<img src="<?php echo $imagem; ?>" class="item-galeria <?php echo ($key==0) ? 'active' : ''; ?>"  style="width: 300px;"/>
+			<?php } ?>
 		</div>
+	</div>
 
-		<script>
-			(function() {
+	<!-- buttons -->
+	<div>
+		<?php foreach($botoes as $key => $botao) { ?>
+			<img src="<?php echo($botao); ?>" class=" <?php echo ($key==0) ? 'botao-voltar' : 'botao-avancar'; ?>"  style="width: 50px;"><?php } ?>
+	</div>
 
-			    var imagens = $('.item-galeria');
+	<script>
+		(function() {
 
-			    var indiceAtivo;
+			var imagens = $('.item-galeria');
 
-			    var numeroImagnes = imagens.length;
+			var indiceAtivo;
+			var numeroImagnes = imagens.length;
+			var proximoAtivo;
 
-			    var proximoAtivo;
+			imagens.each(function(index, value) {
+				if( $(value).hasClass('active') ) {
+					indiceAtivo = index;
+				}
+			});
 
-			    imagens.each(function(index, value) {
-					if( $(value).hasClass('active') ) {
-					    indiceAtivo = index;
-					}
-				});
+			// Avançar imagem.
+			$('.botao-avancar').click(function() {
+				var imagens = $('.item-galeria');
+				$(imagens[indiceAtivo]).removeClass('active');
 
-				// Avançar imagem.
-			    $('.botao-avancar').click(function() {
-			        var imagens = $('.item-galeria');
-			        $(imagens[indiceAtivo]).removeClass('active');
+				if( indiceAtivo == numeroImagnes-1 ) {
+					proximoAtivo = 0;
+				} else {
+					proximoAtivo = indiceAtivo + 1;
+				}
 
-			        if( indiceAtivo == numeroImagnes-1 ) {
-					    proximoAtivo = 0;
-					} else {
-			            proximoAtivo = indiceAtivo + 1;
-					}
+				$(imagens[proximoAtivo]).addClass('active');
+				indiceAtivo = proximoAtivo;
+			});
 
-                    $(imagens[proximoAtivo]).addClass('active');
-                    indiceAtivo = proximoAtivo;
-				});
+			// Voltar imagem.
+			$('.botao-voltar').click(function() {
+				var imagens = $('.item-galeria');
+				$(imagens[indiceAtivo]).removeClass('active');
 
-			    // Voltar imagem.
-			    $('.botao-voltar').click(function() {
-                    var imagens = $('.item-galeria');
-                    $(imagens[indiceAtivo]).removeClass('active');
+				if( indiceAtivo == 0 ) {
+					proximoAtivo = numeroImagnes-1;
+				} else {
+					proximoAtivo = indiceAtivo - 1;
+				}
 
-                    if( indiceAtivo == 0 ) {
-                        proximoAtivo = numeroImagnes-1;
-                    } else {
-                        proximoAtivo = indiceAtivo - 1;
-                    }
+				$(imagens[proximoAtivo]).addClass('active');
+				indiceAtivo = proximoAtivo;
+			});
 
-                    $(imagens[proximoAtivo]).addClass('active');
-                    indiceAtivo = proximoAtivo;
-				});
+		})();
+	</script>
 
-			})();
-		</script>
-
-	</body>
-
-</html>
