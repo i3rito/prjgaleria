585bca8f9e67577c4419b61b4cb3550174c490db
i3rito <matheusi3rito@gmail.com>
2019-10-09 11:13:58 -0300

    Criam os templates.

---
+++ application/views/templates/header.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+
+	<title>H legria</title>
+
+	<script src="/assets/js/jquery.min.js"></script>
+
+	<style>
+		.item-galeria{
+			display: none;
+		}
+		.active {
+			border: 1px solid red;
+			display: inline;
+		}
+		#viewport{
+			height: 172px;
+			width: 300px;
+		}
+	</style>
+
+
+</head>
+
+<meta charset="UTF-8">
+
+
+<body>
+
+	<a href="welcome" >Versão do bootstrap</a>
+
+	<h1><?php echo $title; ?></h1>
