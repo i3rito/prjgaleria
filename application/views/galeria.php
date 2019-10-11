2c42c84c0acf0b71e386e78c6052d1655a40046d
i3rito <matheusi3rito@gmail.com>
2019-10-11 13:40:46 -0300

    Cria o formulário para adicionar galerias.

---
+++ application/views/galeria.php
@@ -0,0 +1,8 @@
+<div>
+
+	<form action="<?php echo base_url('adicionarGaleria/create') ?>" method="post">
+		<input type="text" name="nome" placeholder="Nome da Galeria" /><br>
+		<button type="submit">Criar nova galeria</button>
+	</form>
+
+</div>
