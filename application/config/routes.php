433b1895c0462f98f02ea9d69448da032448c986
i3rito <matheusi3rito@gmail.com>
2019-10-11 13:36:04 -0300

    Configura as rotas.

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['gallery'] = 'gallery/index';
+$route['gallery'] = 'gallery';
+$route['home'] = 'home';
+$route['AdicionarGaleria/create'] = 'adicionargaleria/nova';
+$route['AdicionarImagem'] = 'adicionarimagem';
+$route['AdicionarImagem/create'] = 'adicionarimagem/create';
+$route['gallery/create'] = 'gallery/create';
